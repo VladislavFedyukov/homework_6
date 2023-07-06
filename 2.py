6990874d1cf20c70849d90dc065abe83418bebf8
Vladislav <vladislav.fedyukov@gmail.com>
2023-07-06 10:06:23 +0300

    добавление кода

--- 2.py
+++ 2.py
@@ -4,3 +4,21 @@
 # Программа получает на вход восемь пар чисел, каждое число от 1 до 8 - координаты 8 ферзей. 
 # Если ферзи не бьют друг друга верните истину, а если бьют - ложь.
 
+n = 8
+x = []
+y = []
+for i in range(n):
+    new_x, new_y = [int(s) for s in input().split()]
+    x.append(new_x)
+    y.append(new_y)
+
+correct = True
+for i in range(n):
+    for j in range(i + 1, n):
+        if x[i] == x[j] or y[i] == y[j] or abs(x[i] - x[j]) == abs(y[i] - y[j]):
+            correct = False
+
+if correct:
+    print('NO')
+else:
+    print('YES')
